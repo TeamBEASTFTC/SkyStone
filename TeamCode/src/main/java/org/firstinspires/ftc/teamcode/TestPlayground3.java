92e073111df818c594f25a8240b2862be360d422
Felix <felix.p.hall@gmail.com>
2019-11-12 16:23:18 +1000

    Wrote new foundation program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPlayground3.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="TestPlayground3", group="Tests")
+public class TestPlayground3 extends LinearOpMode{
+    double power=0.5;
+    boolean redAlliance = false;
+
+    HardwareSetup choppy = new HardwareSetup();
+
+
+    @Override
+    public void runOpMode() {
+        choppy.init(hardwareMap, telemetry, false, false);
+
+        waitForStart();
+        choppy.moveForwBack(power,3000,true);
+        choppy.lowerFoundationClips();
+        sleep(1000);
+        choppy.moveForwBack(power,1500, false);
+        choppy.raiseFoundationClips();
+        choppy.moveForwBack(power,1000,false);
+
+        choppy.shuffle(power,1500,redAlliance);
+        choppy.moveForwBack(power,2500,true);
+        choppy.rotate90(true, 2000);
+        choppy.shuffle(power,2000,redAlliance);
+        choppy.moveForwBack(power,4000,true);
+
+
+    }
+}
