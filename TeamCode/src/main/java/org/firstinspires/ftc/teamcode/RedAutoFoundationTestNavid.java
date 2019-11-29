80d9f8c1c345c5d9700fb66962fa2493678e0ca3
Felix <felix.p.hall@gmail.com>
2019-11-29 12:51:49 +1000

    merged from master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
@@ -16,12 +16,12 @@ public class RedAutoFoundationTestNavid extends LinearOpMode{
 
         choppy.init(hardwareMap, telemetry, true, true);
 
-        choppy.setFoundationClipPower(-0.02);
+//        choppy.setFoundationClipPower(-0.02);
 
         waitForStart();
 //        choppy.shuffle(0.5, 500, !blueAlliance);
         choppy.moveForwBackEncoder(0.5, -2, true, !blueAlliance);
-        choppy.releaseCapstone(););
+        choppy.releaseCapstone();
         // moving to drop capstone
         choppy.moveForwBackEncoder(0.5,16.75,true,true);
 
