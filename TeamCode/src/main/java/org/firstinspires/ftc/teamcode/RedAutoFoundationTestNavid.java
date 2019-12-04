3693e87d2bdb914626737f953ed17e0469d15c78
Julie Blaga <iuliablaga@outlook.com>
2019-12-04 14:30:50 +1000

    tested (slightly) the foundation auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name="RedAutoFoundationTestNavid", group="Tests")
 public class RedAutoFoundationTestNavid extends LinearOpMode{
 //    double power=0.5;
-    boolean blueAlliance = true;
+    boolean blueAlliance = false;
     double power;
     HardwareSetup choppy = new HardwareSetup();
     @Override
@@ -20,30 +20,30 @@ public class RedAutoFoundationTestNavid extends LinearOpMode{
 
         waitForStart();
 //        choppy.shuffle(0.5, 500, !blueAlliance);
-        choppy.moveForwBackEncoder(0.75, 8,true, !blueAlliance);
+        choppy.moveForwBackEncoder(0.75, 8,true, false);
         choppy.rotateEncoder(0.5, 400,false, false);
-        choppy.moveForwBackEncoder(0.75, 2, true,true);
         choppy.releaseCapstone();
         // moving to drop capstone
-        choppy.moveForwBackEncoder(0.75,14,true,false);
-        choppy.rotateEncoder(0.75,400,false,false);
-        choppy.moveForwBackEncoder(0.25,24.5,true,true);
+        choppy.moveForwBackEncoder(0.75,20,true,false);
+        choppy.rotateEncoder(0.5,400,false,false);
+        choppy.moveForwBackEncoder(0.75,20,true,true);
+        choppy.moveForwBackEncoder(0.25,4.5,true,true);
         //moving to grab the foundation.
         choppy.lowerFoundationClips();
         choppy.moveForwBackEncoder(0.5,20,true,false);
         choppy.moveForwBackEncoder(0.25, 2, true, false);
         choppy.raiseFoundationClips();
         //move the foundation
-        choppy.moveForwBackEncoder(0.75,6,true,false);
-        choppy.rotateEncoder(0.75,400,false,false);
-        choppy.moveForwBackEncoder(0.75,20,true,false);
+        choppy.moveForwBackEncoder(0.75,10,true,false);
+        choppy.rotateEncoder(0.5,400,false,false);
+        choppy.moveForwBackEncoder(0.75,15,true,false);
         //obstruct the foundation
         choppy.rotateEncoder(0.5,400,false,false);
         choppy.moveForwBackEncoder(1,60,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.5,78.25,true,false);
+        choppy.moveForwBackEncoder(0.5,15.75,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.5,35,true,false);
+        choppy.moveForwBackEncoder(0.5,43.5,true,false);
         choppy.rotateEncoder(0.5, 400, false, false);
         choppy.moveForwBackEncoder(0.5, 35, true, false);
         //move the foundation back
