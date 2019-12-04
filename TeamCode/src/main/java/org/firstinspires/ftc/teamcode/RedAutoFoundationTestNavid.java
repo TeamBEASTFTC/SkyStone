5b1fe3a73fa3e081b8e5e26c15b9f179aaf7be82
Julie Blaga <iuliablaga@outlook.com>
2019-12-04 13:02:52 +1000

    Rotation now checks against average.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
@@ -20,25 +20,26 @@ public class RedAutoFoundationTestNavid extends LinearOpMode{
 
         waitForStart();
 //        choppy.shuffle(0.5, 500, !blueAlliance);
-        choppy.moveForwBackEncoder(0.5, 8,true, !blueAlliance);
+        choppy.moveForwBackEncoder(0.75, 8,true, !blueAlliance);
         choppy.rotateEncoder(0.5, 400,false, false);
-        choppy.moveForwBackEncoder(0.5, 2, true,true);
+        choppy.moveForwBackEncoder(0.75, 2, true,true);
         choppy.releaseCapstone();
         // moving to drop capstone
-        choppy.moveForwBackEncoder(0.5,16.75,true,false);
-        choppy.rotateEncoder(0.5,400,false,false);
+        choppy.moveForwBackEncoder(0.75,14,true,false);
+        choppy.rotateEncoder(0.75,400,false,false);
         choppy.moveForwBackEncoder(0.25,24.5,true,true);
         //moving to grab the foundation.
         choppy.lowerFoundationClips();
-        choppy.moveForwBackEncoder(0.25,22,true,false);
+        choppy.moveForwBackEncoder(0.5,20,true,false);
+        choppy.moveForwBackEncoder(0.25, 2, true, false);
         choppy.raiseFoundationClips();
         //move the foundation
-        choppy.moveForwBackEncoder(0.5,6,true,false);
-        choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.5,18,true,false);
+        choppy.moveForwBackEncoder(0.75,6,true,false);
+        choppy.rotateEncoder(0.75,400,false,false);
+        choppy.moveForwBackEncoder(0.75,20,true,false);
         //obstruct the foundation
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.5,60,true,false);
+        choppy.moveForwBackEncoder(1,60,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
         choppy.moveForwBackEncoder(0.5,78.25,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
