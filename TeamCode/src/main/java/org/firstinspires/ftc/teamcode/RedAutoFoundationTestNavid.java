1c39bc5f59f55951148797a60f6584cb6eeb5551
Julie Blaga <iuliablaga@outlook.com>
2019-12-05 19:14:39 +1100

    full autonomous testing with navigation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoFoundationTestNavid.java
@@ -20,38 +20,38 @@ public class RedAutoFoundationTestNavid extends LinearOpMode{
 
         waitForStart();
 //        choppy.shuffle(0.5, 500, !blueAlliance);
-        choppy.moveForwBackEncoder(0.75, 8,true, false);
+        choppy.moveForwBackEncoder(0.5, 8,true, false);
         choppy.rotateEncoder(0.5, 400,false, false);
         choppy.releaseCapstone();
         // moving to drop capstone
 
-        choppy.moveForwBackEncoder(0.75,35,true,false);
+        choppy.moveForwBackEncoder(0.5,35,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.75,20,true,true);
+        choppy.moveForwBackEncoder(0.5,20,true,true);
         choppy.moveForwBackEncoder(0.25,4.5,true,true);
         //moving to grab the foundation.
 
         choppy.lowerFoundationClips();
-        choppy.moveForwBackEncoder(0.75,12,true,false);
+        choppy.moveForwBackEncoder(0.5,12,true,false);
         choppy.raiseFoundationClips();
         //move the foundation
 
-        choppy.moveForwBackEncoder(0.75,10,true,false);
+        choppy.moveForwBackEncoder(0.5,10,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.75,26,true,false);
+        choppy.moveForwBackEncoder(0.5,26,true,false);
         //obstruct the foundation ready to move it back
 
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.75,35,true,false);
+        choppy.moveForwBackEncoder(0.5,35,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.75,20,true,false);
+        choppy.moveForwBackEncoder(0.5,20,true,false);
         choppy.rotateEncoder(0.5,400,false,false);
-        choppy.moveForwBackEncoder(0.75,35,true,false);
+        choppy.moveForwBackEncoder(0.5,35,true,false);
         //push foundation
 
-        choppy.moveForwBackEncoder(0.75,35,true,true);
+        choppy.moveForwBackEncoder(0.5,35,true,true);
         choppy.rotateEncoder(0.5, 600, false, false);
-        choppy.moveForwBackEncoder(0.75, 35, true, false);
+        choppy.moveForwBackEncoder(0.5, 35, true, false);
         //move to get navigating points
 
 
