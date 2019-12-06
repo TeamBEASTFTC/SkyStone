97a838a7306d2b289a3f1feb43328232b41dfc38
Felix <felix.p.hall@gmail.com>
2019-12-06 10:35:07 +1000

    Added parking auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavigationCloseToNeutralBridge.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="NavigationCloseToNeutralBridge", group="Tests")
+public class NavigationCloseToNeutralBridge extends LinearOpMode{
+    //TO do:
+    //Write a catch for if it gets stuck in using the encoders
+    double power=0.5;
+    //    boolean redAlliance = false;
+    boolean blueAlliance = false;
+
+
+    HardwareSetup choppy = new HardwareSetup();
+
+
+    @Override
+    public void runOpMode() {
+        choppy.init(hardwareMap, telemetry, false, true,true);
+
+
+        choppy.init(hardwareMap, telemetry, true, false,blueAlliance);
+
+        waitForStart();
+        choppy.moveForwBackEncoder(0.25,36, true, false);
+        choppy.rotateEncoder(0.25,((400/90)*6),false, blueAlliance);
+        choppy.shuffleEncoder(0.5,(24/0.8), true,!blueAlliance);
+        choppy.rotateEncoder(0.5,390, false, blueAlliance);
+        choppy.releaseCapstone();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavigationCloseToWall.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.HardwareSetup;
+
+@Autonomous(name="NavigationCloseToWall", group="Auto")
+public class NavigationCloseToWall extends LinearOpMode {
+
+    boolean blueAlliance = false;
+
+
+    HardwareSetup choppy = new HardwareSetup();
+
+
+    @Override
+    public void runOpMode() {
+        choppy.init(hardwareMap, telemetry, false, true, true);
+
+
+        choppy.init(hardwareMap, telemetry, true, false, blueAlliance);
+
+        waitForStart();
+        choppy.moveForwBackEncoder(0.25, 12, true, false);
+        choppy.rotateEncoder(0.25, ((400 / 90) * 6), false, blueAlliance);
+        choppy.shuffleEncoder(0.5, (24 / 0.8), true, !blueAlliance);
+        choppy.rotateEncoder(0.5,390, false, blueAlliance);
+
+        choppy.releaseCapstone();
+    }
+}
